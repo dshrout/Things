f26e355941221e74634bc8ca303a2eef58e8a29c
Don Shrout <dshrout@gmail.com>
2022-02-19 09:48:26 -0600

    things api is working correctly and adding angular client

---
+++ client/src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,36 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { faUser, faPowerOff, faCopy } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css'],
+})
+export class NavBarComponent implements OnInit {
+  isCollapsed = true;
+  faUser = faUser;
+  faPowerOff = faPowerOff;
+  faCopy = faCopy;
+
+  constructor(
+    public auth: AuthService,
+    @Inject(DOCUMENT) private doc: Document
+  ) {}
+
+  ngOnInit() {}
+
+  loginWithRedirect() {
+    this.auth.loginWithRedirect();
+  }
+
+  logout() {
+    this.auth.logout({ returnTo: this.doc.location.origin });
+  }
+
+  copyIdToken = async () => {
+    // const claims = await this.auth.getId
+    // navigator.clipboard.writeText(accessKey);
+  }
+}
